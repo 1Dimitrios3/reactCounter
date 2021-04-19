0dcd7fee23d1d2c9a5fa8fdc464f178c972ae734
1Dimitrios3 <dimitrismavrokefalos@gmail.com>
2021-04-19 23:40:37 +0300

    playing around with react. Counter creation part 1

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Counter from './components/counter'
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
